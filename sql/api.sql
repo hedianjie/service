04635216db0040db838840c76ae181f82f83a797
hedianjie <1163486362@qq.com>
2019-11-15 16:58:30 +0800

    s

--- sql/api.sql
+++ sql/api.sql
@@ -10,25 +10,25 @@ Target Server Type    : MYSQL
 Target Server Version : 80013
 File Encoding         : 65001
 
-Date: 2019-11-14 16:53:27
+Date: 2019-11-15 16:51:38
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `project`
+-- Table structure for `api`
 -- ----------------------------
-DROP TABLE IF EXISTS `project`;
-CREATE TABLE `project` (
-  `id` bigint(255) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `project_name` char(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '项目名称',
-  `project_port` int(5) DEFAULT '80' COMMENT '缂佹棏鍨拌ぐ娑㈠矗?',
-  `project_url` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '1270.0.01' COMMENT '閸︽澘娼?',
+DROP TABLE IF EXISTS `api`;
+CREATE TABLE `api` (
+  `id` bigint(25) NOT NULL AUTO_INCREMENT,
+  `api` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '' COMMENT '接口地址',
+  `api_desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '接口描述',
+  `method` int(2) DEFAULT NULL COMMENT '请求方式',
+  `request` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `response` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- ----------------------------
--- Records of project
+-- Records of api
 -- ----------------------------
-INSERT INTO `project` VALUES ('3', 'API接口管理系统', '7455', 'http://127.0.0.1');
-INSERT INTO `project` VALUES ('6', 'API2接口文档', '7453', 'http://127.1.1.1');
